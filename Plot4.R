c84d3936a2826f6694aa673fc063975df3f52762
Jbuckstat <buckstat@gmail.com>
2021-03-23 12:19:52 -0400

    Project1 EDA

--- Plot4.R
+++ Plot4.R
@@ -16,16 +16,16 @@ par(mar=c(4.5, 4.5, 2, 2))
 par(mfrow = c(2,2))
 with(hpc, {
   
-  plot(Time,Global_active_power, type = "l", xlab = "", ylab = "Global Active Power", cex.lab=0.8)
+  plot(Time,Global_active_power, type = "l", xlab = "", ylab = "Global Active Power", cex.lab=0.9)
   
   plot(Time,Voltage, type = "l", xlab = "datetime", ylab = "Voltage", cex.lab = .9)
   
-  plot(Time, Sub_metering_1, type = "l", col = "black", xlab = "", ylab = "Energy sub metering", cex.lab = .8)
+  plot(Time, Sub_metering_1, type = "l", col = "black", xlab = "", ylab = "Energy sub metering", cex.lab = .9)
   lines(Time, Sub_metering_2,  col = "red")
   lines(Time, Sub_metering_3, col = "blue")
-  legend( "topright", c("Sub_metering_1","Sub_metering_2", "Sub_metering_3"),col=c("black","red","blue"), lty=c(1,1,1), cex = .7)
+  legend( "topright", c("Sub_metering_1","Sub_metering_2", "Sub_metering_3"),col=c("black","red","blue"), lty=c(1,1,1), cex = .8)
   
-  plot(Time, Global_reactive_power, type = "l", xlab = "datetime", cex.lab = .8)
+  plot(Time, Global_reactive_power, type = "l", xlab = "datetime", cex.lab = .9)
   
   
 })
