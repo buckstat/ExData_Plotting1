d80a463a68fd0f1aa3d6a1a9e1c5b65fc592bd48
Jbuckstat <buckstat@gmail.com>
2021-03-23 13:53:42 -0400

    Project1 EDA

--- Plot4.R
+++ Plot4.R
@@ -1,15 +1,20 @@
 #Plot 4
-
+# load necessary Library
 library(tidyverse)
 
-setwd("~/coursera/EDA")
+#Read in the data file, set na to "?" (deals with the numeric values) and get the necessary 2 days
+
+setwd("~/coursera/EDAproject1")
 dataIn <- read.table("household_power_consumption.txt", header=TRUE, sep=";", stringsAsFactors=FALSE,dec = ".", na.strings = "?")
 hpc <- dataIn[dataIn$Date %in% c("1/2/2007","2/2/2007") ,]
 
+# convert the time to POSIX times
 
 hpc$Date <- strptime(hpc$Date, "%d/%m/%Y")
 a <- paste(hpc$Date, hpc$Time, collapse = NULL)
 hpc$Time <- strptime(a,format="%Y-%m-%d %H:%M:%S")
+
+# Create the appropriate graph, and print it as a png files
 
 png("plot4.png", width=480, height=480)
 par(mar=c(4.5, 4.5, 2, 2))
